7e5e387a02334079536c39773da9f1ff0e5b63c4
gunu_h <9nugunu@korea.ac.kr>
2023-11-12 17:50:29 +0900

    rev02) set default value of tb_adder for testing

--- tb_ripple_8bit.sv
+++ tb_ripple_8bit.sv
@@ -1,4 +1,4 @@
-'timescale 1ns / 1ps
+`timescale 1ns / 1ps
 
 module tb_8bit_ripple;
 
@@ -15,20 +15,18 @@ module tb_8bit_ripple;
         .cout(cout)
     );
 
-    # 
-    initial begin
-        $$display("**** Starting Test ****");
-        a = 0; b = 0; cin = 0;
-        passed_tests =0; failed_tests = 0;
-        #5
-        a = 8'b00000001;
-        b = 8'b00000001;
-        cin = 0;
-        #5
+    integer passed_tests = 0;
+    integer failed_tests = 0;
 
-        a = 8'b00000010;
-        b = 8'b00000011;
-        #5;
+    reg [7:0] test_a[9:0];
+    reg [7:0] test_b[9:0];
+    reg test_cin[9:0];
+
+    initial begin
+        $display("**** Starting Test ****");
+        test_a = {8'd1, 8'd2, 8'd3, 8'd4, 8'd5, 8'd6, 8'd7, 8'd8, 8'd9, 8'd10};
+        test_b = {8'd1, 8'd2, 8'd3, 8'd4, 8'd5, 8'd6, 8'd7, 8'd8, 8'd9, 8'd10};
+        test_cin = {1'b0, 1'b1, 1'b0, 1'b1, 1'b0, 1'b1, 1'b0, 1'b1, 1'b0, 1'b1};
 
         for (int i = 0; i < 10; i = i + 1) begin
             a = test_a[i];
@@ -36,7 +34,7 @@ module tb_8bit_ripple;
             cin = test_cin[i];
             #5;
 
-            if (sum == (a+b+cin) && cout == (a+b+cin) >> 8) begin
+            if (sum == (a+b+cin)) begin
                 $display("Test %d successed: a = %d, b = %d, cin = %b, sum = %d, cout = %b", i, a, b, cin, sum, cout);
                 passed_tests = passed_tests + 1;
             end else begin
